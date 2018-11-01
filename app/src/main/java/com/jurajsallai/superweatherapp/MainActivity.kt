e26f06eefc9c6a23fecaad167ece20c8bf5e5616
Juraj.Sallai <juraj.sallai@embedit.cz>
2018-11-01 16:53:41 +0100

    checking listview

--- app/src/main/java/com/jurajsallai/superweatherapp/MainActivity.kt
+++ app/src/main/java/com/jurajsallai/superweatherapp/MainActivity.kt
@@ -1,79 +1,78 @@
 package com.jurajsallai.superweatherapp
 
-import android.content.Context
-import android.databinding.DataBindingUtil
-import android.net.ConnectivityManager
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.jurajsallai.superweatherapp.databinding.ActivityMainBinding
-import com.jurajsallai.superweatherapp.datamodel.CurrentWeatherModel
-import com.jurajsallai.superweatherapp.dialogfragment.AlertDialogFragment
+import android.view.View
+import android.widget.Button
+import android.widget.ListView
+
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import android.widget.Toast
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
+import kotlinx.android.synthetic.main.activity_weather.*
+
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var city: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // TODO custom button style + vyriesit klikanie
 
-        val city = enterCityEditText
+        /***TESTING - SOMETHING LIKE SQL***/
+        var listOfButtons: MutableList<View> = mutableListOf()
+        val citiesFromSql = arrayOf("Levice", "London", "Nitra")
+        var id = 0
+        for (city in citiesFromSql) {
+            val buttonX = Button(this)
+            buttonX.id = id
+            buttonX.text = city
+    //        cityLayout.addView(buttonX)
+            lv.addView(buttonX)
+            // adding buttons (views) to List
+            listOfButtons.add(id, buttonX)
+            //lv.addFooterView(buttonX)
+            id.inc()
+        }
+
+        lv.setOnItemClickListener { parent, view, position, id ->
+            titleTextView.text = view.id.toString()
+        }
+
+//        listView.setOnItemClickListener(OnItemClickListener { parent, view, position, id ->
+//            val o = prestListView.getItemAtPosition(position)
+//            val str = o as prestationEco //As you are using Default String Adapter
+//            Toast.makeText(baseContext, str.getTitle(), Toast.LENGTH_SHORT).show()
+//        })
+
+
+        /******/
+
         saveButton.setOnClickListener {
-            loadData(city.text.toString())
+
+            city = enterCityEditText.text.toString()
+            val buttonX = Button(this)
+            buttonX.text = city
+       //     cityLayout.addView(buttonX)
+            // TODO nacitanie miest z databazy,po SAVE ulozenie a nacitanie znova
         }
 
+//        button1.setOnClickListener {
+//            city = enterCityEditText.text.toString()
+//            checkWeatherByCity(city)
+//        }
+
     }
 
-    private fun loadData(city: String) {
-        if (isOnline()) {
-            val weatherAPI = WeatherService.Factory.create()
-            val call = weatherAPI.getWeatherByCityName(city) as Call<CurrentWeatherModel>
-            call.enqueue(object : Callback<CurrentWeatherModel> {
-
-                override fun onFailure(call: Call<CurrentWeatherModel>?, t: Throwable?) {
-                    onFailure()
-                }
-
-                override fun onResponse(call: Call<CurrentWeatherModel>?, response: Response<CurrentWeatherModel>) {
-                    if (response.isSuccessful) {
-                        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
-                        binding.weather = response.body()
-//                  TODO when have correct POJO with iconId
-//                        runOnUiThread {
-//                            drawable = resources.getDrawable(response.body().getIconId)
-//                        }
-                        onSuccess(response.body())
-                    } else {
-                        onFailure()
-                    }
-                }
-            })
-        }
+    private fun checkWeatherByCity(city: String) {
+        val intent = Intent(this, WeatherActivity::class.java)
+        intent.putExtra("CITY", city)
+        startActivity(intent)
     }
 
-    private fun onFailure() {
-        AlertDialogFragment().show(supportFragmentManager, getString(R.string.error_dialog_tag))
-    }
 
-    private fun onSuccess(currentWeatherModel: CurrentWeatherModel?) {
-        val tempCel = (currentWeatherModel?.main?.temp?.minus(273.15)).toString() + "° C"
-
-        savedCitiesTextView.text = tempCel
-    }
-
-    private fun isOnline(): Boolean {
-        val manager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val networkInfo = manager.activeNetworkInfo
-        val networkStatus: Boolean = networkInfo?.isConnected ?: false
-
-        if (networkStatus) {
-            return networkStatus
-        } else {
-            AlertDialogFragment().show(supportFragmentManager, getString(R.string.error_dialog_tag))
-        }
-        return networkStatus
-    }
 }
---
+++ app/src/main/java/com/jurajsallai/superweatherapp/WeatherActivity.kt
@@ -0,0 +1,74 @@
+package com.jurajsallai.superweatherapp
+
+import android.content.Context
+import android.databinding.DataBindingUtil
+import android.net.ConnectivityManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.jurajsallai.superweatherapp.databinding.ActivityWeatherBinding
+import com.jurajsallai.superweatherapp.datamodel.CurrentWeatherModel
+import com.jurajsallai.superweatherapp.dialogfragment.AlertDialogFragment
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class WeatherActivity : AppCompatActivity() {
+
+    private lateinit var city: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+        city = intent.getStringExtra("CITY")
+        loadData(city)
+    }
+
+    private fun loadData(city: String) {
+        if (isOnline()) {
+            val weatherAPI = WeatherService.Factory.create()
+            val call = weatherAPI.getWeatherByCityName(city) as Call<CurrentWeatherModel>
+            call.enqueue(object : Callback<CurrentWeatherModel> {
+
+                override fun onFailure(call: Call<CurrentWeatherModel>?, t: Throwable?) {
+                    onFailure()
+                }
+
+                override fun onResponse(call: Call<CurrentWeatherModel>?, response: Response<CurrentWeatherModel>) {
+                    if (response.isSuccessful) {
+                        val binding: ActivityWeatherBinding = DataBindingUtil.setContentView(this@WeatherActivity, R.layout.activity_weather)
+                        binding.weather = response.body()
+//                  TODO when have correct POJO with iconId
+//                        runOnUiThread {
+//                            drawable = resources.getDrawable(response.body().getIconId)
+//                        }
+                        onSuccess(response.body())
+                    } else {
+                        onFailure()
+                    }
+                }
+            })
+        }
+    }
+
+    private fun onFailure() {
+        AlertDialogFragment().show(supportFragmentManager, getString(R.string.error_dialog_tag))
+    }
+
+    private fun onSuccess(currentWeatherModel: CurrentWeatherModel?) {
+        val tempCel = (currentWeatherModel?.main?.temp?.minus(273.15)).toString() + "° C"
+        //  savedCitiesTextView.text = tempCel // TODO open new Activity for given city
+    }
+
+    private fun isOnline(): Boolean {
+        val manager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = manager.activeNetworkInfo
+        val networkStatus: Boolean = networkInfo?.isConnected ?: false
+
+        if (networkStatus) {
+            return networkStatus
+        } else {
+            AlertDialogFragment().show(supportFragmentManager, getString(R.string.error_dialog_tag))
+        }
+        return networkStatus
+    }
+}
